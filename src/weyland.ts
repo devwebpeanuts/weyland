4924653a330f146070cf1e5f7967f51505ff8dc9
Romain RENAUD <devwebpeanuts@gmail.com>
2021-03-23 00:46:52 +0100

    feat: add module settings to enable/disable effects

--- src/weyland.ts
+++ src/weyland.ts
@@ -1,4 +1,52 @@
+Hooks.once('init', () => {
+    game.settings.register('weyland', 'flickering', {
+        name: game.i18n.localize('WEYLAND.flickering'),
+        hint: game.i18n.localize('WEYLAND.flickeringHint'),
+        scope: 'client',
+        type: Boolean,
+        default: true,
+        config: true,
+        onChange: () => {
+            location.reload();
+        },
+    })
+
+    game.settings.register('weyland', 'screenDoor', {
+        name: game.i18n.localize('WEYLAND.screenDoor'),
+        hint: game.i18n.localize('WEYLAND.screenDoorHint'),
+        scope: 'client',
+        type: Boolean,
+        default: true,
+        config: true,
+        onChange: () => {
+            location.reload();
+        },
+    });
+
+    game.settings.register('weyland', 'scanline', {
+        name: game.i18n.localize('WEYLAND.scanline'),
+        hint: game.i18n.localize('WEYLAND.scanlineHint'),
+        scope: 'client',
+        type: Boolean,
+        default: true,
+        config: true,
+        onChange: () => {
+            location.reload();
+        },
+    });
+})
+
 Hooks.once('ready', function() {
-    const scanline = $('<div>').addClass('scanline');
-    $('body').append(scanline)
+    if(game.settings.get('weyland', 'scanline')) {
+        const scanline: JQuery<HTMLElement> = $('<div>').addClass('scanline');
+        $('body').append(scanline)
+    }
+
+    if(game.settings.get('weyland', 'flickering')) {
+        $('body').addClass('flickering')
+    }
+
+    if(game.settings.get('weyland', 'screenDoor')) {
+        $('body').addClass('screen-door')
+    }
 })
